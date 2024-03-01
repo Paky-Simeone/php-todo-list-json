78e9c0ec910040cfe26cb3d598280fa2617c31f6
Paky-Simeone <pasquale.simeone.46@gmail.com>
2024-03-01 16:01:32 +0100

    add backend

---
+++ backend/api/serie-list.php
@@ -0,0 +1,3 @@
+<?php
+$serie_list_content = file_get_contents('../data/listaserie.json');
+var_dump($serie_list_content);
--- frontend/main.js
+++ frontend/main.js
@@ -3,9 +3,10 @@ const { createApp } = Vue;
 const app = createApp({
     data(){
         return{
+            title: 'Lista Serie Tv',
             
-        }
-    }
-})
+        };
+    },
+});
 
 app.mount('#app');
